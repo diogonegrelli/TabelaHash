1ed75b5196c1b02c235e7446ae15788fec21b62b
bell <128303802+belljamyle@users.noreply.github.com>
2023-10-16 00:46:02 -0300

    Finalizado

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,11 @@
 public class Main {
     public static void main(String[] args) {
 
-        TabelaHash clientes = new TabelaHash();
+        TabelaHash clientes1 = new TabelaHash();
+        TabelaHash clientes2 = new TabelaHash();
 
         Cliente vazio = new Cliente(-1, "");
-        clientes.iniciarTabela(vazio);
+        clientes1.iniciarTabela(vazio);
 
         Cliente c10 = new Cliente(546, "Ana");
         Cliente c11 = new Cliente(234, "Carlos");
@@ -18,25 +19,48 @@ public class Main {
         Cliente c17 = new Cliente(447, "Ameinda");
         Cliente c18 = new Cliente(675, "Claudio");
 
+        // insere utilizando o primeiro tratamento
+        clientes1.inserirCliente1(c17);
+        clientes1.inserirCliente1(c13);
+        clientes1.inserirCliente1(c10);
+        clientes1.inserirCliente1(c14);
+        clientes1.inserirCliente1(c11);
+        clientes1.inserirCliente1(c15);
+        clientes1.inserirCliente1(c18);
 
-        clientes.inserirCliente1(c17);
-        clientes.inserirCliente1(c13);
-        clientes.inserirCliente1(c10);
-        clientes.inserirCliente1(c12);
-        clientes.inserirCliente1(c11);
-        //clientes.inserirCliente1(c16);
-        clientes.inserirCliente1(c15);
-        //clientes.inserirCliente1(c14);
-        clientes.inserirCliente1(c18);
+        clientes1.printarTabela();
 
+        clientes1.removerCliente1(945);
+        clientes1.buscarCliente1(675);
 
-        clientes.printarTabela();
+        clientes1.printarTabela();
 
+        clientes2.iniciarTabela(vazio);
 
-       // clientes.buscarCliente(367);
-        clientes.removerCliente(234);
-        clientes.removerCliente(675);
-        clientes.printarTabela();
+        // insere utilizando o segundo tratamento
+        clientes2.inserirCliente2(c17);
+        clientes2.inserirCliente2(c13);
+        clientes2.inserirCliente2(c10);
+        clientes2.inserirCliente2(c14);
+        clientes2.inserirCliente2(c11);
+        clientes2.inserirCliente2(c15);
+        clientes2.inserirCliente2(c18);
+
+        clientes2.printarTabela();
+
+        clientes2.removerCliente2(945);
+        clientes2.buscarCliente2(675);
+
+        clientes2.printarTabela();
+
+        System.out.println("A função de busca do primeiro tratamento realizou " + clientes1.iteracoes1 + " iterações");
+        System.out.println("A função de busca do primeiro tratamento realizou " + clientes2.iteracoes2 + " iterações");
+
+        if(clientes1.iteracoes1 < clientes2.iteracoes2) {
+            System.out.println("O primeiro tratamento é mais eficiente!");
+        } else {
+            System.out.println("O segundo tratamento é mais eficiente!");
+        }
 
     }
 
--- src/TabelaHash.java
+++ src/TabelaHash.java
@@ -1,6 +1,3 @@
-import java.util.Hashtable;
-import java.util.LinkedList;
-
 public class TabelaHash {
 
 
@@ -8,6 +5,9 @@ public class TabelaHash {
 
     int size = tabelaHash.length;
     int posicoes = 9;
+    int iteracoes1;
+    int iteracoes2;
+
 
     public void iniciarTabela(Cliente vazio){
         for (int i =0; i< size ; i++){
@@ -25,11 +25,13 @@ public class TabelaHash {
         int idCliente = cliente.getIdCliente();
         int pos = funcaoHash(idCliente);
         if (tabelaHash[pos].getIdCliente() == -1 || tabelaHash[pos].getIdCliente() == -2){
-            tabelaHash[pos] = cliente;}
-        else{
-            while (posicaoColisao <13){
-                if (tabelaHash[posicaoColisao].getIdCliente()==-1 || tabelaHash[posicaoColisao].getIdCliente()==-2){
+            tabelaHash[pos] = cliente;
+            System.out.println("Cliente inserido com sucesso!");
+        } else {
+            while (posicaoColisao < 13) {
+                if (tabelaHash[posicaoColisao].getIdCliente()==-1 || tabelaHash[posicaoColisao].getIdCliente()==-2) {
                     tabelaHash[posicaoColisao] = cliente;
+                    System.out.println("Cliente inserido com sucesso!");
                     return;
                 }
                 posicaoColisao = posicaoColisao +1;
@@ -37,51 +39,93 @@ public class TabelaHash {
         }
     }
 
-    public Cliente buscarCliente(int chave) {
-        int pos = funcaoHash(chave);
-
-            if (tabelaHash[pos].getIdCliente() == -1) {
-                System.out.println("Objeto não encontrado!");
+    public void inserirCliente2 (Cliente cliente) {
+        int idCliente = cliente.getIdCliente();
+        int pos = funcaoHash(idCliente);
+        while (true) {
+            if (tabelaHash[pos].getIdCliente() == -1 || tabelaHash[pos].getIdCliente() == -2) {
+                tabelaHash[pos] = cliente;
+                System.out.println("Cliente inserido com sucesso!");
+                break;
             } else {
-                if (tabelaHash[pos].getIdCliente() == chave) {
-                    System.out.println("Item encontrado!");
-                
-                }else{
-                    for(int i = posicoes; i < size; i++) {
-                        if(tabelaHash[i].getIdCliente() == chave) {
-                            System.out.println("Item Encontrado!");
-                            return tabelaHash[i];
-                        }
-                    }
-            System.out.println("Item não encontrado");
-            return null;
-        }
-            
+                if (pos == size - 1){
+                    pos = 0;
+                } else{
+                    pos += 1;
+                }
             }
-            return tabelaHash[pos];
+        }
+    }
+
+    public Cliente buscarCliente1(int chave) {
+        int pos = funcaoHash(chave);
+        iteracoes1 = 0;
+        if (tabelaHash[pos].getIdCliente() == -1) {
+            System.out.println("Objeto não encontrado!");
+            iteracoes1 += 1;
+        } else {
+            if (tabelaHash[pos].getIdCliente() == chave) {
+                System.out.println("Item encontrado!");
+                iteracoes1 += 1;
+            } else {
+                for(int i = posicoes; i < size; i++) {
+                    if(tabelaHash[i].getIdCliente() == chave) {
+                        System.out.println("Item Encontrado!");
+                        iteracoes1 += 1;
+                        return tabelaHash[i];
+                    }
+                    iteracoes1 += 1;
+                }
+                System.out.println("Item não encontrado");
+                return null;
+            }
+        }
+        return tabelaHash[pos];
+    }
+
+    public Cliente buscarCliente2 (int chave) {
+        iteracoes2 = 0;
+        int pos = funcaoHash(chave);
+        Cliente clienteEncontrado;
+        while (true) {
+            if (tabelaHash[pos].getIdCliente() == chave) {
+                clienteEncontrado = tabelaHash[pos];
+                System.out.println("Cliente encontrado!");
+                break;
+            } else if(tabelaHash[pos].getIdCliente() == -2) {
+                pos += 1;
+            } else if(tabelaHash[pos].getIdCliente() == -1){
+                System.out.println("Cliente encontrado!");
+                clienteEncontrado = null;
+                break;
+            } else {
+                pos += 1;
+            }
+            iteracoes2 += 1;
+        }
+        return clienteEncontrado;
     }
 
     
 
-    public Cliente removerCliente(int chave) {
+    public Cliente removerCliente1(int chave) {
         int pos = funcaoHash(chave);
+        Cliente disponivel = new Cliente(-2, " ");
     
         if (tabelaHash[pos].getIdCliente() == -1) {
             System.out.println("Falha na remoção, item não encontrado!");
             return null;
         } else if (tabelaHash[pos].getIdCliente() == chave) {
-            System.out.println("Cliente: " + tabelaHash[pos].getIdCliente() + "-" + tabelaHash[pos].getNomeCliente());
+            System.out.println("Cliente: " + tabelaHash[pos].getIdCliente() + " - " + tabelaHash[pos].getNomeCliente());
             System.out.println("Removido com sucesso!");
-            tabelaHash[pos].setIdCliente(-2);
-            tabelaHash[pos].setNomeCliente(" ");
+            tabelaHash[pos] = disponivel;
             return tabelaHash[pos];
         } else {
             for (int i = posicoes; i < size; i++) {
                 if (tabelaHash[i].getIdCliente() == chave) {
-                    System.out.println("Cliente: " + tabelaHash[i].getIdCliente() + "-" + tabelaHash[i].getNomeCliente());
+                    System.out.println("Cliente: " + tabelaHash[i].getIdCliente() + " - " + tabelaHash[i].getNomeCliente());
                     System.out.println("Removido com sucesso!");
-                    tabelaHash[i].setIdCliente(-2);
-                    tabelaHash[i].setNomeCliente(" ");
+                    tabelaHash[i] = disponivel;
                     return tabelaHash[i];
                 }
             }
@@ -90,7 +134,29 @@ public class TabelaHash {
         System.out.println("Falha na remoção, item não encontrado!");
         return null;
     }
-    
+
+    public Cliente removerCliente2(int chave) {
+        Cliente disponivel = new Cliente(-2, " ");
+        int pos = funcaoHash(chave);
+        Cliente clienteRemovido;
+        while (true) {
+            if (tabelaHash[pos].getIdCliente() == chave) {
+                clienteRemovido = tabelaHash[pos];
+                System.out.println("Cliente " + tabelaHash[pos].getIdCliente() + " - " + tabelaHash[pos].getNomeCliente() + " removido com sucesso!");
+                tabelaHash[pos] = disponivel;
+                break;
+            } else if(tabelaHash[pos].getIdCliente() == -2) {
+                pos += 1;
+            } else if(tabelaHash[pos].getIdCliente() == -1) {
+                System.out.println("Cliente não encontrado!");
+                clienteRemovido = null;
+                break;
+            } else {
+                pos += 1;
+            }
+        }
+        return clienteRemovido;
+    }
 
     public void printarTabela(){
         System.out.println("--------------------");
@@ -101,6 +167,5 @@ public class TabelaHash {
             System.out.println("-------------------------");
         }
     }
-
 
 }
